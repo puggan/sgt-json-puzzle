662a6bb6d427fcaf69c685f64acb0f10974af3b3
Thorbjörn Sundragon <puggan@spiro.se>
2019-07-07 01:28:30 +0200

    PHPWraper Solo

--- phpwrapers/unruly.php
+++ phpwrapers/unruly.php
@@ -5,8 +5,10 @@
 	chdir(__DIR__ . '/..');
 	exec('bin/unrulysolver', $o);
 
+	$dificulties = ['e' => 'Easy', 'n' => 'Normal'];
+
 	$config = preg_replace('/.* /', '', $o[0]);
-	if(!preg_match('/^(?<w2>\d+)x(?<h2>\d+)(?<u>u?)d(?<d>.)$/', $config, $m)) {
+	if(!preg_match('/^(?<w2>\d+)x(?<h2>\d+)(?<x>u)?d(?<d>.)$/', $config, $m)) {
 		header('HTTP/1.1 500 Failed generations');
 		die('false');
 	}
@@ -17,9 +19,9 @@
 	$data->settings->columns = $m['w2'];
 	$data->settings->rows = $m['h2'];
 	$data->settings->unique = !empty($m['u']);
-	$data->settings->difficulty = $m['d'] === 'e' ? 'Easy' : 'Normal';
+	$data->settings->difficulty = $dificulties[$m['d']] ?? $dificulties['e'];
 	$data->id = $data->settings->columns . 'x' .$data->settings->rows . ':' . substr($o[1 + $data->settings->rows], 9);
-	$data->seed = $config . '#' . substr($o[2 + $data->settings->rows], 9);
+	$data->seed = $config . '#' . substr($o[2 + $data->settings->rows], 6);
 	$data->state = [];
 
 	$chr_table = '.01';
@@ -32,7 +34,7 @@
 		$data->state[] = array_map($chr_lookup, explode(' ', $o[$row_nr]));
 	}
 
-	if(empty($_GET['debug']))
+	if(0 && empty($_GET['debug']))
 	{
 		echo json_encode($data);
 	} else {
