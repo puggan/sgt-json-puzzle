01e7bd4ebd9f78e7823c6d6f7c3ed79d48d0c8bc
Puggan Sundragon <puggan@sundragon.se>
2019-07-07 13:16:50 +0200

    Fifteen

---
+++ phpwrapers/fifteen.php
@@ -0,0 +1,39 @@
+<?php
+
+	header('Content-Type: application/json');
+
+	chdir(__DIR__ . '/..');
+	exec('bin/fifteensolver', $o);
+
+	[$name, $seed] = explode(': ', $o[2]);
+	[$name, $id] = explode(': ', $o[1], 2);
+	[$name, $config] = explode(': ', $o[0]);
+
+
+	if(!preg_match('/^(?<w2>\d+)x(?<h2>\d+)$/', $config, $m))
+	{
+		header('HTTP/1.1 500 Failed generations');
+		echo $config, PHP_EOL;
+		die('false');
+	}
+
+	$data = (object) [];
+	$data->id = $config . ':' . $id;
+	$data->name = $name;
+	$data->settings = (object) [];
+	$data->settings->columns = $m['w2'];
+	$data->settings->rows = $m['h2'];
+	$data->seed = $config . '#' . $seed;
+	$data->state = (object) [];
+
+	$clues = explode(',', $id);
+	$data->state = array_chunk($clues, $data->settings->columns);
+
+	if(empty($_GET['debug']) && ($argv[1] ?? '') !== '-v')
+	{
+		echo json_encode($data);
+	} else {
+		$data->debug = $o;
+		echo json_encode($data, 128*3);
+	}
+
